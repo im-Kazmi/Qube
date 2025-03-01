ffe6b28d3cf965e33459b3ac49910ec52bd24154
im-Kazmi <abidkazmi729@gmail.com>
2025-03-01 23:38:58 +0500

    setup ui and base clien

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,8 @@
 import { postRouter } from "./routers/post";
-import { appsRouter } from "./routers/apps";
 import { createCallerFactory, createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
-  apps: appsRouter,
 });
 
 // export type definition of API
